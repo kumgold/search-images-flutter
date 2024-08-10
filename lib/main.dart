eb85c99e90705a5291700c69e9c720693dc8957a
JunghunKum <123260423+kumgold@users.noreply.github.com>
2024-08-10 23:44:09 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -50,10 +50,6 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
       body: screen[_index],
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
